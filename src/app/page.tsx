850e561a9f0d6cff9abef6e02ff287d841fc0ba9
Suryansh Anand <spirit@anandsur.com>
2025-03-29 16:42:22 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+
+export default function HomePage() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center ">
+			<Button>Click me</Button>
+		</main>
+	);
+}
